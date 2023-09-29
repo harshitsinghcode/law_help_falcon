795b67ef635f36b63cc2810668bfb81da66afcc3
Amritansh Sharma <amritansh.code@gmail.com>
2023-09-29 20:34:15 +0530

    added undertrial home page basic

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Nyaya Sahaya',
       theme: ThemeData.dark(),
-      home: LoginPage(),
+      home: const LoginPage(),
       debugShowCheckedModeBanner: false,
     );
   }
