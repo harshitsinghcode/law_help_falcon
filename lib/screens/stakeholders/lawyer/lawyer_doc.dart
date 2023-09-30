2f3bd808bde9d5051c7437d26cd0f623c41c7857
amspsingh04 <sp.singh.150604@gmail.com>
2023-09-30 13:04:40 +0530

    Some changes

--- lib/screens/stakeholders/lawyer/lawyer_doc.dart
+++ lib/screens/stakeholders/lawyer/lawyer_doc.dart
@@ -9,8 +9,59 @@ class LawyerDocument extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Document Generation'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              // Handle logout button pressed
+            },
+            icon: Icon(Icons.logout),
+          ),
+        ],
       ),
-      body: LogoutButton(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(child: _buildCard('Card 1')),
+                  Expanded(child: _buildCard('Card 2')),
+                ],
+              ),
+              SizedBox(height: 20.0), // Add some spacing between rows
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(child: _buildCard('Card 3')),
+                  Expanded(child: _buildCard('Card 4')),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCard(String cardTitle) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(
+              cardTitle,
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            Text('Card content goes here'),
+          ],
+        ),
+      ),
     );
   }
 }
